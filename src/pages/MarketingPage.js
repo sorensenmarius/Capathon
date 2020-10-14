39f6d1119a709d08f80cc702408abd930f1c896e
Ove <ove.kristian.jorgensen@gmail.com>
2020-10-14 20:30:36 +0200

    make it rain

--- src/pages/MarketingPage.js
+++ src/pages/MarketingPage.js
@@ -2,10 +2,43 @@
 import React, { useState } from "react";
 import Flyer from "../resources/flyer.png";
 import "../styles/marketStyles.css";
+import "../styles/money.css"
 
 const MarketingPage = () => {
   return (
     <div className="outerImg">
+      <div class="container">
+        <div class="loader">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
       <h1>LATEST FROM THE MARKETING TEAM!</h1>
       <img src={Flyer} alt="new" />
     </div>
